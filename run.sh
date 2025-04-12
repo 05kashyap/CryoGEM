33c02e9d80d0950120662d7ca5f5703a6423128f
05kashyap <aryankashyapnaveen@gmail.com>
2025-04-13 00:50:48 +0530

    restricted to single gpu usage

--- run.sh
+++ run.sh
@@ -8,17 +8,17 @@
 
 # rm -rf testing/data.zip
 
-python -m cryogem gen_data --mode homo --device cuda:0 \
-  --input_map testing/data/exp_abinitio_volumes/densitymap.10028.90.mrc \
-  --save_dir save_images/ddpm_data/train \
-  --n_micrographs 100 --particle_size 90 --mask_threshold 0.9
+# python -m cryogem gen_data --mode homo --device cuda:0 \
+#   --input_map testing/data/exp_abinitio_volumes/densitymap.10028.90.mrc \
+#   --save_dir save_images/ddpm_data/train \
+#   --n_micrographs 100 --particle_size 90 --mask_threshold 0.9
 
-python -m cryogem gen_data --mode homo --device cuda:0 \
-  --input_map testing/data/exp_abinitio_volumes/densitymap.10028.90.mrc \
-  --save_dir save_images/ddpm_data/test \
-  --n_micrographs 50 --particle_size 90 --mask_threshold 0.9
+# python -m cryogem gen_data --mode homo --device cuda:0 \
+#   --input_map testing/data/exp_abinitio_volumes/densitymap.10028.90.mrc \
+#   --save_dir save_images/ddpm_data/test \
+#   --n_micrographs 50 --particle_size 90 --mask_threshold 0.9
 
-python -m cryogem esti_ice --apix 5.36 --input_dir save_images/ddpm_data/train/mics_mrc --save_dir save_images/esti_ice/Ribosome\(10028\)/ --output_len 1024         
+# python -m cryogem esti_ice --apix 5.36 --input_dir save_images/ddpm_data/train/mics_mrc --save_dir save_images/esti_ice/Ribosome\(10028\)/ --output_len 1024         
 
 # Diffusion timesteps reduced from 1000
 python -m cryogem ddpm_pipeline --dataset "Ribosome(10028)" \
@@ -28,5 +28,4 @@ python -m cryogem ddpm_pipeline --dataset "Ribosome(10028)" \
   --epochs 10 \
   --timesteps 250 \
   --batch_size 1 \
-  --output_dir save_images/ddpm_experiment \
-  --gpu_ids 0
+  --output_dir save_images/ddpm_experiment
