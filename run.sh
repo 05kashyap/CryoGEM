bbfdc6f0328024c0c5d8891da67109d70450e148
05kashyap <aryankashyapnaveen@gmail.com>
2025-04-12 23:02:14 +0530

    Added run.sh to run on server

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 # This script is used to run the Python script with the specified arguments.after repo has been cloned
 
-curl -L -o testing/data.zip https://www.kaggle.com/api/v1/datasets/download/aryankashyapnaveen/cryogem-test
+# curl -L -o testing/data.zip https://www.kaggle.com/api/v1/datasets/download/aryankashyapnaveen/cryogem-test
 
-unzip testing/data.zip -d testing/ # Unzip the downloaded file to the testing directory
+# unzip testing/data.zip -d testing/ # Unzip the downloaded file to the testing directory
 
-rm -rf testing/data.zip
+# rm -rf testing/data.zip
 
 python -m cryogem gen_data --mode homo --device cuda:0 \
   --input_map testing/data/exp_abinitio_volumes/densitymap.10028.90.mrc \
@@ -18,13 +18,9 @@ python -m cryogem gen_data --mode homo --device cuda:0 \
   --save_dir save_images/ddpm_data/test \
   --n_micrographs 50 --particle_size 90 --mask_threshold 0.9
 
-python -m cryogem esti_ice --apix 5.36 \                
-  --input_dir save_images/ddpm_data/train/mics_mrc \                     
-  --save_dir save_images/esti_ice/Ribosome\(10028\)/ \
-  --output_len 1024         
+python -m cryogem esti_ice --apix 5.36 --input_dir save_images/ddpm_data/train/mics_mrc --save_dir save_images/esti_ice/Ribosome\(10028\)/ --output_len 1024         
 
-python -m cryogem ddpm_pipeline \           
-  --dataset "Ribosome(10028)" \
+python -m cryogem ddpm_pipeline --dataset "Ribosome(10028)" \
   --gpu 0 \
   --training_samples 100 \
   --testing_samples 50 \
